0d8206808537605771a5048dd74ccadcc8ae34aa
Amil Masic <amil.masic@gmail.com>
2018-09-13 20:39:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,31 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |x,y|
+    y <=> x
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |left, right|
+    left.length <=> right.length
+  end
+end
+
+# def swap_elements(array)
+#   array.sort do |x,y|
+# 
+#     x <=> y
+#   end
+#   # end
+# end
+
+def reverse_array(array)
+  array.reverse
+end
+
+# def kesha_maker(array)
+# 
+# end
